c6b6395279f1cb1d73a1fcd915340c04945f7249
bluedear <128768429+bluedar@users.noreply.github.com>
2023-07-07 13:18:18 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const bubble_container = document.querySelector(".bubble_container");
+const chat_form_msg = document.querySelector(".chat_form_msg");
+
+const chat_form_submit = document.querySelector(".chat_form_submit");
+let chatValue;
+
+const makeTalk = (chatValue) => {
+  const createDiv = document.createElement("div");
+  createDiv.classList.add("preview");
+  createDiv.classList.add("bubble_preview");
+  createDiv.classList.add("my");
+  createDiv.innerHTML = `
+            <div class="preview_col">
+              <div class="preview_pic"></div>
+            </div>
+            <div class="preview_col bubble_wrap">
+              <h4 class="preview_title">유저이름</h4>
+              <div class="bubble_content">
+                <p class="bubble">${chatValue}</p>
+                <span class="bubble_time">오후 2:33</span>
+              </div>
+            </div>
+    `;
+  bubble_container.append(createDiv);
+};
+
+const upText = () => {
+  chatValue = chat_form_msg.value;
+  if (chatValue !== "") {
+    makeTalk(chatValue);
+  }
+  chat_form_msg.value = "";
+};
+
+chat_form_submit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  upText();
+});
+
+chat_form_msg.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (e.key === "Enter") {
+    upText();
+  }
+});
